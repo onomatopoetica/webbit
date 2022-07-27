8ed29bb05eb3992a3ebf4ea9ddf2a7849eac5b2a
onomatopoetica <hi.jen@icloud.com>
2022-07-27 16:15:00 -0400

    Fixed  rubocop/lint offenses

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root 'home#index'
 end
