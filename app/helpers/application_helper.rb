f9d31590761d56a1464af0099427b86c935c4720
onomatopoetica <hi.jen@icloud.com>
2023-02-14 16:33:05 -0500

    Linting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     options[:title] ||= name.underscore.humanize
     options[:aria] = true
     options[:nocomment] = true
-    options[:class] = options.fetch(:styles, "fill-current text-gray-500")
+    options[:class] = options.fetch(:styles, 'fill-current text-gray-500')
     filename = "#{name}.svg"
     inline_svg_tag(filename, options)
   end
